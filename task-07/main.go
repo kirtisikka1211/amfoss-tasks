ce4b936d60a266ef0ce9545a8e41b34422755bec
kirtisikka1211 <kirtisikka972@gmail.com>
2022-12-14 13:59:19 +0530

    task7

---
+++ task-07/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var i int
+	for i = 0; i < 100; i++ {
+		fmt.Printf(" %d\n", i)
+	}
+	var x int
+	for x = 100; x > 0; x-- {
+		fmt.Printf("  %d\n", x)
+	}
+
+}
